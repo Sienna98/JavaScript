f0061f0e49e2b629d3083b38c99eb9cd887a463a
sieun.lee <se.lee@purple.io>
2023-04-24 14:14:16 +0900

    배열 예시문제

--- 배열.js
+++ 배열.js
@@ -98,19 +98,40 @@ let animals = [
 ];
 
 //어레이에 마지막 아이템 “Zebra” 제거하기
+animals.pop();
+// console.log(animals);
 
 //주어진 어레이에 “Dog” 추가하기
+animals.push("Dog");
+// console.log(animals);
 
 //주어진 어레이에 “Mosquito”,“Mouse”,“Mule” 추가하기
+animals.push("Mosquito", "Mouse", "Mule");
+// console.log(animals);
 
 //해당 어레이에는 "Human"이 있는가?
+//false
+// console.log(animals.includes("Human"));
 
 //해당 어레이에는 “Cat” 이 있는가?
+// true
+// console.log(animals.includes("Cat"));
 
 //"Red deer"을 "Deer"로 바꾸시오
+animals.splice(animals.indexOf("Red deer"), 1, "Deer");
+// console.log(animals);
 
 //"Spider"부터 3개의 아이템을 기존 어레이에서 제거하시오
+animals.splice(animals.indexOf("Spider"), 3);
+// console.log(animals);
 
 //"Tiger"이후의 값을 제거하시오
+animals.splice(animals.indexOf("Tiger"));
+// console.log(animals);
 
 //"B"로 시작되는 아이템인 "Baboon"부터 "Bison"까지 가져와 새로운 어레이에 저장하시오
+let newAnimals = animals.slice(
+  animals.indexOf("Baboon"),
+  animals.indexOf("Bison") + 1
+);
+// console.log(newAnimals);
